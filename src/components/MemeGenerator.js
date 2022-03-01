003f12e8748180c08f61640b1495f350780a45df
liezsm <liez.dev@gmail.com>
2022-03-01 13:06:22 +0800

    Added API for meems
    Added the external api for memes data

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -4,14 +4,56 @@ class MemeGenerator extends React.Component {
 
     constructor(props){
         super(props)
+        this.state ={
+            memes : '',
+            random: ''
+        }
+        this.handleClick = this.handleClick.bind(this)
 
     }
+
+    handleClick(){
+          const randomIndex = Math.floor( Math.random() * this.state.memes.length)
+     
+      console.log(this.state.memes[randomIndex])
+      this.setState({ random: this.state.memes[randomIndex]})
+
+    }
+    componentDidMount() {
+
+const loadData = async () =>{
+
+    try {
+
+        const url ='https://api.imgflip.com/get_memes';
+        const resp = await fetch(url)
+        const data = await resp.json()
+        const {data:{memes}} = data;
+      
+        this.setState( {memes: memes})
+    }catch(err){
+        console.log(err)
+    }
+
+}
+
+loadData()
+
+    }
+
+
 
     render() {
 
         return (
             <div className="meme_container">
-                meme
+                <button className='meme_container_btn'
+                onClick={this.handleClick}>
+                    <span>Generate meme</span></button>
+                <div className="meme__result" style={{border: "2px solid red"}}>
+                <h1>{this.state.random.name}</h1>
+                <img src={this.state.random.url} alt="random meme" />
+                </div>
             </div>
         )
     }
